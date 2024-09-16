ff3f18fec08f888874856abda661956c40b79e23
Payo <castellanoalvarez48@gmail.com>
2024-09-16 19:54:51 -0300

    Clase del 16/09

---
+++ assets/main.js
@@ -0,0 +1,15 @@
+let nombre = "Pedro"
+
+let apellido = "Castellano"
+
+let edad = prompt("ingrese su edad")
+
+nombre = "Pablo"
+apellido = "Alvarez"
+
+console.log(nombre,apellido)
+console.log("tu edad es",edad)
+alert("tu edad es " + edad)
+
+
+console.log(`${nombre} ${apellido} \ntu edad es: ${edad}`)
